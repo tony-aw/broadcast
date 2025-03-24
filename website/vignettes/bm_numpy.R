ee4e1ed95cf94d53dd7540feaaf314e70d0373f0
Tony <tony_a_wilkes@outlook.com>
2025-03-24 20:08:53 +0100

    Initial testing

--- website/vignettes/bm_numpy.R
+++ website/vignettes/bm_numpy.R
@@ -1,8 +1,10 @@
+
 # set-up ====
 library(broadcast)
 library(reticulate)
 np <- import("numpy", convert = FALSE)
 gc <- import("gc", convert = FALSE)
+gc$disable()
 get_times <- function(obj, j) {
   nms <- names(res$expression)
   j <- which(nms == j)
@@ -10,24 +12,22 @@ get_times <- function(obj, j) {
   times <- obj$time[[j]][idx]
   return(times)
 }
-gc$disable()
 
-# loop
-median_bc <- median_np <- q1_bc <- q1_np <- q3_bc <- q3_np <- vector("numeric", 8)
+
+# loop 2d ====
+gc()
+dimsizes <- seq(1000L, 3000L,  by = 200L)
+niter <- length(dimsizes)
+median_bc <- median_np <- q1_bc <- q1_np <- q3_bc <- q3_np <- vector("numeric", niter)
 counter <- 1L
-target_len <- 9e6
 
-for(i in 2:9) {
+for(i in seq_along(dimsizes)) {
   print(i)
-  n <- round(target_len^(1/i)) |> as.integer()
-  len <- n^i
-  cat("i = ", i, "\n")
-  cat("n = ", n, "\n")
-  cat("len = ", len, "\n")
-  x.dims <- rep(c(n, 1L), i - 1)[1:i]
-  y.dims <- rep(c(1L, n), i - 1)[1:i]
-  a.dims <- rt$r_to_py(as.list(x.dims))
-  b.dims <- rt$r_to_py(as.list(y.dims))
+  n <- dimsizes[i]
+  x.dims <- c(n, 1L)
+  y.dims <- c(1L, n)
+  a.dims <- r_to_py(as.list(x.dims))
+  b.dims <- r_to_py(as.list(y.dims))
   
   npa <- np$random$random_sample(a.dims)
   npb <- np$random$random_sample(b.dims)
@@ -38,9 +38,12 @@ for(i in 2:9) {
     broadcast = bc.num(a, b, "+"),
     `numpy (NO conversion to R)` = npa + npb,
     check = FALSE,
-    min_iterations = 100
+    min_iterations = 200
   )
   bc_all <- get_times(res, "broadcast")
+  if(length(bc_all) < 100) {
+    stop("too few benchmarks for 'R'")
+  }
   np_all <- get_times(res, "numpy (NO conversion to R)")
   median_bc[counter] <- median(bc_all)
   median_np[counter] <- median(np_all)
@@ -53,114 +56,263 @@ for(i in 2:9) {
 }
 
 
+
+
 save(
-  median_bc, median_np, q1_bc, q3_bc, q1_np, q3_np,
-  file = "bm_numpy_loop.RData"
+  dimsizes, median_bc, median_np, q1_bc, q3_bc, q1_np, q3_np,
+  file = "bm_numpy_loop_2d.RData"
 )
 
 
-# BigSmall ====
-n <- 200L
-npa <- np$random$rand(n, n, 1L, n)
-npb <- np$random$rand(n, n, 1L, 1L)
 
-a <- array(rnorm(100), c(n, n, 1L, n))
-b <- array(rnorm(100), c(n, n, 1, 1))
+# loop 3d ====
+gc()
+dimsizes <- seq(20L, 150L,  by = 10L)
+niter <- length(dimsizes)
+median_bc <- median_np <- q1_bc <- q1_np <- q3_bc <- q3_np <- vector("numeric", niter)
+counter <- 1L
 
-bm_numpy_bs <- bench::mark(
-  broadcast = bc.num(a, b, "+"),
-  `numpy (no conversion to R)` = npa + npb,
-  check = FALSE,
-  min_iterations = 100,
+for(i in seq_along(dimsizes)) {
+  print(i)
+  n <- dimsizes[i]
+  x.dims <- rep(c(n, 1L), 2)[1:3]
+  y.dims <- rep(c(1L, n), 2)[1:3]
+  a.dims <- r_to_py(as.list(x.dims))
+  b.dims <- r_to_py(as.list(y.dims))
+  
+  npa <- np$random$random_sample(a.dims)
+  npb <- np$random$random_sample(b.dims)
+  a <- array(runif(100), x.dims)
+  b <- array(runif(100), y.dims)
+  
+  res <- bench::mark(
+    broadcast = bc.num(a, b, "+"),
+    `numpy (NO conversion to R)` = npa + npb,
+    check = FALSE,
+    min_iterations = 200
+  )
+  bc_all <- get_times(res, "broadcast")
+  if(length(bc_all) < 100) {
+    stop("too few benchmarks for 'R'")
+  }
+  np_all <- get_times(res, "numpy (NO conversion to R)")
+  median_bc[counter] <- median(bc_all)
+  median_np[counter] <- median(np_all)
+  q1_bc[counter] <- quantile(bc_all, 0.25)
+  q3_bc[counter] <- quantile(bc_all, 0.75)
+  q1_np[counter] <- quantile(np_all, 0.25)
+  q3_np[counter] <- quantile(np_all, 0.75)
+  
+  counter <- counter + 1L
+}
+
+
+
+
+save(
+  dimsizes, median_bc, median_np, q1_bc, q3_bc, q1_np, q3_np,
+  file = "bm_numpy_loop_3d.RData"
 )
-summary(bm_numpy_bs)
-ggplot2::autoplot(bm_numpy_bs)
-save(bm_numpy_bs, file = "bm_numpy_bs.RData")
+
+
+# loop 4d ====
+gc()
+dimsizes <- seq(10L, 55L,  by = 5L)
+niter <- length(dimsizes)
+median_bc <- median_np <- q1_bc <- q1_np <- q3_bc <- q3_np <- vector("numeric", niter)
+counter <- 1L
+
+for(i in seq_along(dimsizes)) {
+  print(i)
+  n <- dimsizes[i]
+  x.dims <- rep(c(n, 1L), 2)[1:4]
+  y.dims <- rep(c(1L, n), 2)[1:4]
+  a.dims <- r_to_py(as.list(x.dims))
+  b.dims <- r_to_py(as.list(y.dims))
+  
+  npa <- np$random$random_sample(a.dims)
+  npb <- np$random$random_sample(b.dims)
+  a <- array(runif(100), x.dims)
+  b <- array(runif(100), y.dims)
+  
+  res <- bench::mark(
+    broadcast = bc.num(a, b, "+"),
+    `numpy (NO conversion to R)` = npa + npb,
+    check = FALSE,
+    min_iterations = 200
+  )
+  bc_all <- get_times(res, "broadcast")
+  if(length(bc_all) < 100) {
+    stop("too few benchmarks for 'R'")
+  }
+  np_all <- get_times(res, "numpy (NO conversion to R)")
+  median_bc[counter] <- median(bc_all)
+  median_np[counter] <- median(np_all)
+  q1_bc[counter] <- quantile(bc_all, 0.25)
+  q3_bc[counter] <- quantile(bc_all, 0.75)
+  q1_np[counter] <- quantile(np_all, 0.25)
+  q3_np[counter] <- quantile(np_all, 0.75)
+  
+  counter <- counter + 1L
+}
 
 
 
-# Orthogonal Vectors ====
-n <- 2000L
-npa <- np$random$rand(n, 1L)
-npb <- np$random$rand(1L, n)
-a <- array(rnorm(n), c(n, 1))
-b <- array(rnorm(n), c(1, n))
 
-bm_numpy_ortho <- bench::mark(
-  broadcast = bc.num(a, b, "+"),
-  `numpy (no conversion to R)` = npa + npb,
-  check = FALSE,
-  min_iterations = 100,
+save(
+  dimsizes, median_bc, median_np, q1_bc, q3_bc, q1_np, q3_np,
+  file = "bm_numpy_loop_4d.RData"
 )
-summary(bm_numpy_ortho)
-ggplot2::autoplot(bm_numpy_ortho)
-save(bm_numpy_ortho, file = "bm_numpy_ortho.RData")
 
 
 
-# mergeable ====
 
-n <- 60L
-npa <- np$random$rand(n, 1L, 1L, n)
-npb <- np$random$rand(n, n, n, 1L)
+# loop 5d ====
+gc()
+dimsizes <- seq(5L, 25L,  by = 5L)
+niter <- length(dimsizes)
+median_bc <- median_np <- q1_bc <- q1_np <- q3_bc <- q3_np <- vector("numeric", niter)
+counter <- 1L
 
-a <- array(rnorm(100), c(n, 1L, 1L, n))
-b <- array(rnorm(100), c(n, n, n, 1L))
+for(i in seq_along(dimsizes)) {
+  print(i)
+  n <- dimsizes[i]
+  x.dims <- rep(c(n, 1L), 3)[1:5]
+  y.dims <- rep(c(1L, n), 3)[1:5]
+  a.dims <- r_to_py(as.list(x.dims))
+  b.dims <- r_to_py(as.list(y.dims))
+  
+  npa <- np$random$random_sample(a.dims)
+  npb <- np$random$random_sample(b.dims)
+  a <- array(runif(100), x.dims)
+  b <- array(runif(100), y.dims)
+  
+  res <- bench::mark(
+    broadcast = bc.num(a, b, "+"),
+    `numpy (NO conversion to R)` = npa + npb,
+    check = FALSE,
+    min_iterations = 200
+  )
+  bc_all <- get_times(res, "broadcast")
+  if(length(bc_all) < 100) {
+    stop("too few benchmarks for 'R'")
+  }
+  np_all <- get_times(res, "numpy (NO conversion to R)")
+  median_bc[counter] <- median(bc_all)
+  median_np[counter] <- median(np_all)
+  q1_bc[counter] <- quantile(bc_all, 0.25)
+  q3_bc[counter] <- quantile(bc_all, 0.75)
+  q1_np[counter] <- quantile(np_all, 0.25)
+  q3_np[counter] <- quantile(np_all, 0.75)
+  
+  counter <- counter + 1L
+}
 
-bm_numpy_mer <- bench::mark(
-  broadcast = bc.num(a, b, "+"),
-  `numpy (no conversion to R)` = npa + npb,
-  check = FALSE,
-  min_iterations = 200,
+
+
+
+save(
+  dimsizes, median_bc, median_np, q1_bc, q3_bc, q1_np, q3_np,
+  file = "bm_numpy_loop_5d.RData"
 )
-summary(bm_numpy_mer)
-ggplot2::autoplot(bm_numpy_mer)
-save(bm_numpy_mer, file = "bm_numpy_mer.RData")
+
+
+
+# loop 6d ====
+gc()
+dimsizes <- seq(1L, 15L,  by = 2L)
+niter <- length(dimsizes)
+median_bc <- median_np <- q1_bc <- q1_np <- q3_bc <- q3_np <- vector("numeric", niter)
+counter <- 1L
+
+for(i in seq_along(dimsizes)) {
+  print(i)
+  n <- dimsizes[i]
+  x.dims <- rep(c(n, 1L), 3)[1:6]
+  y.dims <- rep(c(1L, n), 3)[1:6]
+  a.dims <- r_to_py(as.list(x.dims))
+  b.dims <- r_to_py(as.list(y.dims))
+  
+  npa <- np$random$random_sample(a.dims)
+  npb <- np$random$random_sample(b.dims)
+  a <- array(runif(100), x.dims)
+  b <- array(runif(100), y.dims)
+  
+  res <- bench::mark(
+    broadcast = bc.num(a, b, "+"),
+    `numpy (NO conversion to R)` = npa + npb,
+    check = FALSE,
+    min_iterations = 200
+  )
+  bc_all <- get_times(res, "broadcast")
+  if(length(bc_all) < 100) {
+    stop("too few benchmarks for 'R'")
+  }
+  np_all <- get_times(res, "numpy (NO conversion to R)")
+  median_bc[counter] <- median(bc_all)
+  median_np[counter] <- median(np_all)
+  q1_bc[counter] <- quantile(bc_all, 0.25)
+  q3_bc[counter] <- quantile(bc_all, 0.75)
+  q1_np[counter] <- quantile(np_all, 0.25)
+  q3_np[counter] <- quantile(np_all, 0.75)
+  
+  counter <- counter + 1L
+}
 
 
 
 
-# irregular array (3d) ====
-
-n <- 200L
-
-npa <- np$random$rand(n, 1L, n)
-npb <- np$random$rand(n, n, 1L)
-
-a <- array(rnorm(100), c(n, 1, n))
-b <- array(rnorm(100), c(n, n, 1))
-
-
-bm_numpy_irr <- bench::mark(
-  broadcast = bc.num(a, b, "+"),
-  `numpy (no conversion to R)` = npa + npb,
-  check = FALSE,
-  min_iterations = 100,
+save(
+  dimsizes, median_bc, median_np, q1_bc, q3_bc, q1_np, q3_np,
+  file = "bm_numpy_loop_6d.RData"
 )
-summary(bm_numpy_irr)
-ggplot2::autoplot(bm_numpy_irr)
-save(bm_numpy_irr, file = "bm_numpy_irr.RData")
+
+
+# loop 7d ====
+gc()
+dimsizes <- seq(1L, 10L,  by = 1L)
+niter <- length(dimsizes)
+median_bc <- median_np <- q1_bc <- q1_np <- q3_bc <- q3_np <- vector("numeric", niter)
+counter <- 1L
+
+for(i in seq_along(dimsizes)) {
+  print(i)
+  n <- dimsizes[i]
+  x.dims <- rep(c(n, 1L), 4)[1:7]
+  y.dims <- rep(c(1L, n), 4)[1:7]
+  a.dims <- r_to_py(as.list(x.dims))
+  b.dims <- r_to_py(as.list(y.dims))
+  
+  npa <- np$random$random_sample(a.dims)
+  npb <- np$random$random_sample(b.dims)
+  a <- array(runif(100), x.dims)
+  b <- array(runif(100), y.dims)
+  
+  res <- bench::mark(
+    broadcast = bc.num(a, b, "+"),
+    `numpy (NO conversion to R)` = npa + npb,
+    check = FALSE,
+    min_iterations = 200
+  )
+  bc_all <- get_times(res, "broadcast")
+  if(length(bc_all) < 100) {
+    stop("too few benchmarks for 'R'")
+  }
+  np_all <- get_times(res, "numpy (NO conversion to R)")
+  median_bc[counter] <- median(bc_all)
+  median_np[counter] <- median(np_all)
+  q1_bc[counter] <- quantile(bc_all, 0.25)
+  q3_bc[counter] <- quantile(bc_all, 0.75)
+  q1_np[counter] <- quantile(np_all, 0.25)
+  q3_np[counter] <- quantile(np_all, 0.75)
+  
+  counter <- counter + 1L
+}
 
 
 
-# large arrays ====
 
-n <- 26L
-npa <- np$random$rand(n, 1L, n, 1L, n)
-npb <- np$random$rand(n, n, 1L, n, 1L)
-
-a.dim <- c(n, rep(c(1L, n), 2))
-b.dim <- c(n, rep(c(n, 1L), 2))
-a <- array(rnorm(100), a.dim)
-b <- array(rnorm(100), b.dim)
-
-bm_numpy_large <- bench::mark(
-  broadcast = bc.num(a, b, "+"),
-  `numpy (no conversion to R)` = npa + npb,
-  check = FALSE,
-  min_iterations = 200,
+save(
+  dimsizes, median_bc, median_np, q1_bc, q3_bc, q1_np, q3_np,
+  file = "bm_numpy_loop_7d.RData"
 )
-summary(bm_numpy_large)
-ggplot2::autoplot(bm_numpy_large)
-save(bm_numpy_large, file = "bm_numpy_large.RData")
-
