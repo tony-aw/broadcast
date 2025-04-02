4b05b78b04a1d63e49b18c4e862687810be0ebba
Tony <tony_a_wilkes@outlook.com>
2025-04-02 19:49:37 +0200

    Ubuntu fix

--- inst/tinytest/bc_num/test-relop_precision.R
+++ inst/tinytest/bc_num/test-relop_precision.R
@@ -1,9 +1,15 @@
 
 # set-up ====
 tol <- sqrt(.Machine$double.eps)
-eps <- tol * 100 # high multiplication, because Ubuntu is STUPID
+eps <- tol
 enumerate <- 0 # to count number of tests performed using iterations in loops
 loops <- 0 # to count number of loops
+
+testfun <- function(x, y, op) {
+  out <- bc.d(x, y, op, prec = tol)
+  out[is.na(out)] <- NA # because Ubuntu doesn't handle NaN and NA properly
+  return(out)
+}
 
 # basic checks ====
 
@@ -26,20 +32,20 @@ equal <- c(rep(TRUE, 3), rep(FALSE, 6), rep(NA, 6))
 smaller <- c(rep(FALSE, 6), rep(TRUE, 3), rep(NA, 6))
 bigger <- c(rep(FALSE, 3), rep(TRUE, 3), rep(FALSE, 3), rep(NA, 6))
 
-expect_equal(bc.d(x, y, "d==") |> as.vector(), equal)
-expect_equal(bc.d(x, y, "d<=") |> as.vector(), equal | smaller)
-expect_equal(bc.d(x, y, "d>=") |> as.vector(), equal | bigger)
-expect_equal(bc.d(x, y, "d!=") |> as.vector(), !equal)
-expect_equal(bc.d(x, y, "d<") |> as.vector(), !equal & smaller)
-expect_equal(bc.d(x, y, "d>") |> as.vector(), !equal & bigger)
+expect_equal(testfun(x, y, "d==") |> as.vector(), equal)
+expect_equal(testfun(x, y, "d<=") |> as.vector(), equal | smaller)
+expect_equal(testfun(x, y, "d>=") |> as.vector(), equal | bigger)
+expect_equal(testfun(x, y, "d!=") |> as.vector(), !equal)
+expect_equal(testfun(x, y, "d<") |> as.vector(), !equal & smaller)
+expect_equal(testfun(x, y, "d>") |> as.vector(), !equal & bigger)
 
 enumerate <- enumerate + 6L
 
 
 # relational checks ====
-expect_equal(bc.d(x, y, "d!="), !(bc.d(x, y, "d==")))
-expect_equal(bc.d(x, y, "d<="), !(bc.d(x, y, "d>")))
-expect_equal(bc.d(x, y, "d>="), !(bc.d(x, y, "d<")))
+expect_equal(testfun(x, y, "d!="), !(testfun(x, y, "d==")))
+expect_equal(testfun(x, y, "d<="), !(testfun(x, y, "d>")))
+expect_equal(testfun(x, y, "d>="), !(testfun(x, y, "d<")))
 
 enumerate <- enumerate + 3L
 
@@ -51,27 +57,27 @@ d1 <- d[, 1] |> as.double() |> as.array()
 d2 <- d[, 2] |> as.double() |> as.array()
 expect_equal(
   as.vector(d1 == d2),
-  bc.d(d1, d2, "d==") |> as.vector()
+  testfun(d1, d2, "d==") |> as.vector()
 )
 expect_equal(
   as.vector(d1 != d2),
-  bc.d(d1, d2, "d!=") |> as.vector()
+  testfun(d1, d2, "d!=") |> as.vector()
 )
 expect_equal(
   as.vector(d1 <= d2),
-  bc.d(d1, d2, "d<=") |> as.vector()
+  testfun(d1, d2, "d<=") |> as.vector()
 )
 expect_equal(
   as.vector(d1 >= d2),
-  bc.d(d1, d2, "d>=") |> as.vector()
+  testfun(d1, d2, "d>=") |> as.vector()
 )
 expect_equal(
   as.vector(d1 < d2),
-  bc.d(d1, d2, "d<") |> as.vector()
+  testfun(d1, d2, "d<") |> as.vector()
 )
 expect_equal(
   as.vector(d1 > d2),
-  bc.d(d1, d2, "d>") |> as.vector()
+  testfun(d1, d2, "d>") |> as.vector()
 )
 
 enumerate <- enumerate + 6L
@@ -87,44 +93,45 @@ all_inf <- which(is.infinite(d1) & is.infinite(d2) & sign(d1) == sign(d2))
 
 expect <- as.vector(d1 == d2)
 expect[all_inf] <- NA
-out <- bc.d(d1, d2, "d==") |> as.vector()
+out <- testfun(d1, d2, "d==") |> as.vector()
 expect_equal(
   expect, out
 )
 
 expect <- as.vector(d1 != d2)
 expect[all_inf] <- NA
-out <- bc.d(d1, d2, "d!=") |> as.vector()
+out <- testfun(d1, d2, "d!=") |> as.vector()
 expect_equal(
   expect, out
 )
 
 expect <- as.vector(d1 <= d2)
 expect[all_inf] <- NA
-out <- bc.d(d1, d2, "d<=") |> as.vector()
+out <- testfun(d1, d2, "d<=") |> as.vector()
 expect_equal(
   expect, out
 )
 
 expect <- as.vector(d1 >= d2)
 expect[all_inf] <- NA
-out <- bc.d(d1, d2, "d>=") |> as.vector()
+out <- testfun(d1, d2, "d>=") |> as.vector()
 expect_equal(
   expect, out
 )
 
 expect <- as.vector(d1 < d2)
 expect[all_inf] <- NA
-out <- bc.d(d1, d2, "d<") |> as.vector()
+out <- testfun(d1, d2, "d<") |> as.vector()
 expect_equal(
   expect, out
 )
 
 expect <- as.vector(d1 > d2)
 expect[all_inf] <- NA
-out <- bc.d(d1, d2, "d>") |> as.vector()
+out <- testfun(d1, d2, "d>") |> as.vector()
 expect_equal(
   expect, out
 )
 
 enumerate <- enumerate + 6L
+
